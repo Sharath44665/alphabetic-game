6d9891b071d1eda793c85784e2558985255d11a6
Sharath <sharathwelcomesyouin@gmail.com>
2022-07-02 17:58:40 +0530

    add feature to only accept letters

--- main.py
+++ main.py
@@ -4,24 +4,24 @@ student_dict = {
 }
 
 # Looping through dictionaries:
-for (key, value) in student_dict.items():
+# for (key, value) in student_dict.items():
     # Access key and value
     # print(key)
     # print(value)
-    pass
+    # pass
 
 import pandas
 
 student_data_frame = pandas.DataFrame(student_dict)
 
 # Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
+# for (index, row) in student_data_frame.iterrows():
     # Access index and row
     # Access row.student or row.score
     # if row.student == "Lily":
     #     print(index)
     # print(row)
-    pass
+    # pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
@@ -33,10 +33,23 @@ nato_words = pandas.read_csv("nato_phonetic_alphabet.csv")
 # TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
 my_dict = {d_val.letter: d_val.code for (d_letter, d_val) in nato_words.iterrows() }
-# print(my_dict)
+print(my_dict)
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 user_val=input("Enter your name: ").upper()
-new_val_list=[my_dict[val] for val in user_val if val in my_dict]
+check_alpha=True
+while check_alpha:
+    try:
+        new_val_list=[my_dict[val] for val in user_val ]
+        check_alpha=False
+    except KeyError:
+        print("please enter only letters")
+        user_val = input("Enter your name: ").upper()
+        check_alpha=True
+    else:
+        new_val_list=[my_dict[val] for val in user_val]
+        # print(new_val_list)
+        check_alpha=False
+
 print(new_val_list)
 # result=[k for (k,v) in my_dict.items() if  k in new_val_list]
 # print(result)
